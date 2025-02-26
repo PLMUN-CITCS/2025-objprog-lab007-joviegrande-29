9d8ee86b40009a27098db2316b5050d0182d146d
joviegrande-29 <grandejovie_bscs@plmun.edu.ph>
2025-02-25 19:00:44 -0800

    Submitting OBJPROG Week 04 - Session 01 - Exercise 01

--- BooleanExpressionsDemo.java
+++ BooleanExpressionsDemo.java
@@ -9,6 +9,8 @@ public class BooleanExpressionsDemo {
 
         boolean isAGreater = a > b;
 
+        boolean areEqual = a == b;
+
         boolean bothTrue = isJavaFun && (a < b);
 
         boolean eitherTrue = isJavaFun || isHomeworkTiring;
@@ -20,6 +22,7 @@ public class BooleanExpressionsDemo {
         System.out.println("Is Java fun? " + isJavaFun);
 System.out.println("Is homework tiring? " + isHomeworkTiring);
 System.out.println("Is 'a' greater than 'b'? " + isAGreater);
+System.out.println("Are 'a' and 'b' equal? " + areEqual);
 System.out.println("Both conditions (isJavaFun && a<b): " + bothTrue);
 System.out.println("Either condition (isJavaFun || isHomeworkTiring): " + eitherTrue);
 System.out.println("Logical NOT of isHomeworkTiring: " + notTrue);
